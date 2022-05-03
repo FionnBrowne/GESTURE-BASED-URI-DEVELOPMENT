8f9abf618f51ebf0ea0c43846026b75a0a7760f2
FionnBrowne <fionnftb@gmail.com>
2022-05-03 18:38:52 +0100

    When enemy hits the wall with movement, they now decend and have some padding

--- Assets/__Scripts/Enemys.cs
+++ Assets/__Scripts/Enemys.cs
@@ -8,6 +8,8 @@ public class Enemys : MonoBehaviour
     [SerializeField] int rows = 5;//rows of enemys, typically there are 5
     [SerializeField] int columns = 11;//columns of enemys, typically there are 11
     [SerializeField] float EnemySpacing = 2;//columns of enemys, typically there are 11
+    [SerializeField] float EnemySpeed = 3.0f;//columns of enemys, typically there are 11
+    private Vector3 direction = Vector2.right;
 
     private void Awake()
     {
@@ -27,5 +29,37 @@ public class Enemys : MonoBehaviour
                 enemy.transform.localPosition = position;//relative to parent positioning
             }
         }
+    }
+    private void Update()//every frame
+    {
+        this.transform.position += this.EnemySpeed * Time.deltaTime * direction;//consistent movement regardless of fps
+
+        Vector3 LEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);//translate view port cordinates to world cordinates
+        Vector3 REdge = Camera.main.ViewportToWorldPoint(Vector3.right);//translate view port cordinates to world cordinates
+
+        foreach (Transform enemy in this.transform)//loops through all child objects attached to this
+        {
+            if (!enemy.gameObject.activeInHierarchy)//if enemy is active or disabled
+            {
+                continue;
+            }
+            //checks if enemys have hit the edge then changes direction and decend a row
+            if (direction == Vector3.right && enemy.position.x >= REdge.x - 1.0f)//-1 for padding so they dont exceed screen
+            {
+                DecendRow();
+            }
+            //checks if enemys have hit the edge then changes direction and decend a row
+            else if (direction == Vector3.left && enemy.position.x <= LEdge.x + 1.0f)//1 for padding so they dont exceed screen
+            {
+                DecendRow();
+            }
+        }
+    }
+    private void DecendRow()
+    {
+        direction.x *= -1.0f;// travel to opposite side of screen
+        Vector3 position = this.transform.position;//get current position
+        position.y -= 1.0f;//decend enemy
+        this.transform.position = position;//assign back to transform
     }
 }
